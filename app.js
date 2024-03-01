08f0db3e70cf954efecc15231158ff4991168155
baljka17 <baljka17@gmail.com>
2024-03-01 19:47:35 +0800

    Calculations

--- app.js
+++ app.js
@@ -12,6 +12,29 @@ var uiController = (function () {
     labelSummary: ".label__summary",
     labelPercent: ".label__badge",
     containerDiv: ".container",
+    expensePercentageLabel: ".expense__list .item__percentage",
+  };
+  var nodeListForeach = function (list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
+  var formatMoney = function (amount, type) {
+    amount = "" + amount;
+    var x = amount.split("").reverse().join("");
+    var y = "";
+    var count = 1;
+    for (let i = 0; i < x.length; i++) {
+      y = y + x[i];
+      if (count % 3 == 0) y += ",";
+      count++;
+    }
+    var z = y.split("").reverse().join("");
+    if (z[0] == ",") z = z.substring(1, z.length - 1);
+    if (type == "add") z = "+ " + z;
+    else z = "-  " + z;
+    return z;
   };
   return {
     getInput: function () {
@@ -24,20 +47,33 @@ var uiController = (function () {
     getDOMstrings: function () {
       return DOMstrings;
     },
+
+    displayPercentages: function (allPercentages) {
+      var elements = document.querySelectorAll(
+        DOMstrings.expensePercentageLabel
+      );
+      nodeListForeach(elements, function (el, index) {
+        el.textContent = allPercentages[index] + "%";
+      });
+    },
     addItemList: function (item, type) {
       // 1. prepare
       var html =
         "<div id='" +
         type +
-        "-##id##' class='item flex gap-2 border-b border-gray-300' > <div class='item__description w-[200px]'>##desc##</div> <div class='item__amount w-[60px] text-right'>$##amount##</div> <div class='item__delete'> <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-5 h-5' > <path stroke-linecap='round' stroke-linejoin='round' d='m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z' /> </svg> </div> </div>";
+        "-##id##' class='item flex gap-2 border-b border-gray-300'><div class='item__description w-[200px]'>##desc##</div><div class='item__percentage text-xs border text-gray-200 rounded-md'></div><div class='item__amount w-[60px] text-right'>##amount##</div><a class='item__delete cursor-pointer'> <svg id='Icons' class='w-5 h-5' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><defs><style>.cls-1{fill:#232323;}</style></defs><path class='cls-1' d='M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm4.707,14.293a1,1,0,1,1-1.414,1.414L12,13.414,8.707,16.707a1,1,0,1,1-1.414-1.414L10.586,12,7.293,8.707A1,1,0,1,1,8.707,7.293L12,10.586l3.293-3.293a1,1,0,1,1,1.414,1.414L13.414,12Z'/></svg> </a> </div>";
       var selector = DOMstrings.incomeList;
       if (type === "sub") selector = DOMstrings.expenseList;
       // 2. HTML
       html = html.replace("##id##", item.id);
       html = html.replace("##desc##", item.desc);
-      html = html.replace("##amount##", item.amount);
+      html = html.replace("##amount##", formatMoney(item.amount, type));
       // 3. Insert into dom
       document.querySelector(selector).insertAdjacentHTML("beforeend", html);
+    },
+    deleteItemList: function (id) {
+      var item = document.getElementById(id);
+      item.parentNode.removeChild(item);
     },
     clearFields: function () {
       var fields = document.querySelectorAll(
@@ -74,6 +110,14 @@ var financialController = (function () {
     this.id = id;
     this.desc = desc;
     this.amount = amount;
+    this.percentage = -1;
+  };
+
+  Expense.prototype.calcPercentage = function (totalIncome) {
+    this.percentage = Math.round((this.amount / totalIncome) * 100);
+  };
+  Expense.prototype.getPercentage = function () {
+    return this.percentage;
   };
 
   var calculateTotal = function (type) {
@@ -113,24 +157,22 @@ var financialController = (function () {
     },
     deleteItem: function (type, id) {
       var ids = data.items[type].map(function (element) {
-        console.log(element);
         return element.id;
       });
-      console.log(data.items);
-      console.log(data.items[type]);
-      // console.log(ids);
-      // var index = ids.indexOf(id);
-      // console.log("deleting " + index);
-      // if (index !== -1) {
-      //   data.items[type].slice(index, 1);
-      // }
+      var index = ids.indexOf(id);
+      if (index !== -1) {
+        //data.items[type] =
+        data.items[type].slice(index, 1);
+      }
+      // console.log(data.items[type]);
     },
     calculate: function () {
       calculateTotal("add");
       calculateTotal("sub");
-
       data.balance = data.totals.add - data.totals.sub;
-      data.percent = Math.round((data.totals.sub / data.totals.add) * 100);
+      if (data.totals.add > 0)
+        data.percent = Math.round((data.totals.sub / data.totals.add) * 100);
+      else data.percent = 0;
     },
     getSummary: function () {
       return {
@@ -142,6 +184,17 @@ var financialController = (function () {
     },
     getData: function () {
       console.log(data);
+    },
+    calculatePercentages: function () {
+      data.items.sub.forEach(function (el) {
+        el.calcPercentage(data.totals.add);
+      });
+    },
+    getPercentages: function () {
+      var percentages = data.items.sub.map(function (el) {
+        return el.getPercentage();
+      });
+      return percentages;
     },
   };
 })();
@@ -156,11 +209,25 @@ var appController = (function (ui, finance) {
 
       // 5. Calculate summary
       finance.calculate();
-      var summary = finance.getSummary();
 
       // 6. Print result
-      ui.showSummary(summary);
+      updateTusuv();
     }
+  };
+
+  var updateTusuv = function () {
+    var summary = finance.getSummary();
+    ui.showSummary(summary);
+    financialController.calculate();
+    var tusuv = financialController.getSummary();
+    uiController.showSummary(summary);
+    // 7. Calculate percentages
+    financialController.calculatePercentages();
+    // 8. Calculate percentages
+    var allPercentages = financialController.getPercentages();
+    // 9. Calculate percentages
+    uiController.displayPercentages(allPercentages);
+    console.log(allPercentages);
   };
   var setupEventListeners = function () {
     var DOM = ui.getDOMstrings();
@@ -180,8 +247,8 @@ var appController = (function (ui, finance) {
           var arr = id.split("-");
           var type = arr[0];
           var itemId = arr[1];
-          console.log(arr);
-          finance.deleteItem(type, itemId);
+          finance.deleteItem(type, parseInt(itemId));
+          ui.deleteItemList(id);
         }
       });
   };
